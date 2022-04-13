69d87dde2d9915895d20420c7c5982cf22b4722a
yodeng <yodeng@tju.edu.cn>
2022-04-13 12:46:14 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+import sys
+import site
+import sysconfig
+import subprocess
+
+from setuptools import setup
+from src.version import __version__
+
+
+def getdes():
+    des = ""
+    if os.path.isfile(os.path.join(os.getcwd(), "README.md")):
+        with open(os.path.join(os.getcwd(), "README.md")) as fi:
+            des = fi.read()
+    return des
+
+
+setup(
+    name="ezmonitor",
+    version=__version__,
+    packages=["ezmonitor"],
+    package_dir={"ezmonitor": "src"},
+    include_package_data=True,
+    install_requires=["matplotlib", "psutil", "pandas"],
+    python_requires='>=2.7.10, <3.10',
+    long_description=getdes(),
+    long_description_content_type='text/markdown',
+    entry_points={
+        'console_scripts': [
+            'ezmntor = ezmonitor.main:main',
+        ]
+    }
+)
---
+++ src/utils.py
@@ -0,0 +1,51 @@
+import os
+import sys
+import math
+import time
+import signal
+import logging
+
+from threading import Thread
+
+
+class ParseSingal(Thread):
+    def __init__(self):
+        super(ParseSingal, self).__init__()
+        signal.signal(signal.SIGINT, self.signal_handler)
+        signal.signal(signal.SIGTERM, self.signal_handler)
+
+    def run(self):
+        time.sleep(1)
+
+    def signal_handler(self, signum, frame):
+        os._exit(signum)
+        # sys.exit(signum)
+
+
+def pgrep(name):
+    pids = []
+    if name:
+        with os.popen("pgrep %s" % name) as fi:
+            pids = fi.read().split()
+    return pids
+
+
+def log_wrapper(logfile=None, level="info", name=None):
+    logger = logging.getLogger(name)
+    if level.lower() == "info":
+        logger.setLevel(logging.INFO)
+    elif level.lower() == "debug":
+        logger.setLevel(logging.DEBUG)
+    f = logging.Formatter('')
+    if logfile is None:
+        h = logging.StreamHandler(sys.stdout)  # default: sys.stderr
+        f = logging.Formatter('[%(levelname)s %(asctime)s] %(message)s')
+    else:
+        h = logging.FileHandler(logfile, mode='w')
+    h.setFormatter(f)
+    logger.addHandler(h)
+    return logger
+
+
+def Gsize(size):
+    return round(size/1024.0/1024/1024, 3)
